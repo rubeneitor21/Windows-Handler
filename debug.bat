64ef31d61d5bad494f7881593d054fba3349d8be
Ruben Garcalia <rubeneitor2@hotmail.com>
2023-12-12 21:19:48 +0100

    Cambios al compilar

--- debug.bat
+++ debug.bat
@@ -1,3 +1,7 @@
+@echo off
+
+echo Compilando...
 g++ window-handler\main.cpp window-handler\headers\rubLogger\rubLogger.cpp window-handler\headers\windowHandler\windowHandler.cpp -o main.exe
 
+echo Lanzando...
 start main.exe
--- production.bat
+++ production.bat
@@ -1,3 +1,7 @@
+@echo off
+echo Compilando...
+
 g++ window-handler\main.cpp window-handler\headers\rubLogger\rubLogger.cpp window-handler\headers\windowHandler\windowHandler.cpp -o main.exe -mwindows
 
-start main.exe
+echo Listo
+@REM start main.exe
